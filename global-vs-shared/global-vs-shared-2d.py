4706112de9a9d17ef1fe9e21e443517b902360c0
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-11-02 08:18:54 -0500

    add global vs shared scripts

---
+++ global-vs-shared/global-vs-shared-2d.py
@@ -0,0 +1,41 @@
+from pycuda import autoinit
+import pycuda.gpuarray as gpuarray
+import pycuda.driver as cuda
+import numpy as np
+from prettytable import PrettyTable
+import sys
+
+mem = sys.argv[1]
+
+if mem == 'global':
+    sys.path.append('../globalmem')
+elif mem == 'shared':
+    sys.path.append('../sharedmem')
+else:
+    print 'Please specify global or shared solver'
+    raise(ValueError)
+
+from neato import NearToeplitzSolver
+
+sizes = (32, 64, 128, 256, 512, 1024, 2048)
+num_systems = (32, 64, 128, 256, 512, 1024, 2048)
+
+start = cuda.Event()
+end = cuda.Event()
+nsteps = 10
+
+table = PrettyTable()
+for N, nrhs in zip(sizes, num_systems):
+    d = np.random.rand(nrhs, N)
+    d_d = gpuarray.to_gpu(d)
+    solver = NearToeplitzSolver(N, nrhs, (1., 2., 1./4, 1., 1./4, 2., 1.))
+    for i in range(nsteps):
+        if i == 1: start.record()
+        solver.solve(d_d)
+    end.record()
+    end.synchronize()
+    table.add_row((N, nrhs, start.time_till(end)/(nsteps-1)))
+table.field_names = ('System size', 'Number of systems', 'Time to solve (ms)')
+table.align = 'l'
+
+print table
