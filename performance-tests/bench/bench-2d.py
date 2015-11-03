c299b9120d00c0b811af851937aaa472b5732f21
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-11-03 15:00:50 -0500

    add bandwidth to benchmark

--- performance-tests/bench/bench-2d.py
+++ performance-tests/bench/bench-2d.py
@@ -10,14 +10,16 @@ from neato import NearToeplitzSolver
 from prettytable import PrettyTable
 
 sizes = (32, 64, 128, 256, 512, 1024, 2048)
-num_systems = (32, 64, 128, 256, 512, 1024, 2048)
+num_systems = (32**2, 64**2, 128**2, 256**2, 512**2, 1024**2, 2048**2)
 
 start = cuda.Event()
 end = cuda.Event()
-nsteps = 10
+nsteps = 100
 
 table = PrettyTable()
 for N, nrhs in zip(sizes, num_systems):
+    in_bytes = N*nrhs*8
+    out_bytes = N*nrhs*8
     d = np.random.rand(nrhs, N)
     d_d = gpuarray.to_gpu(d)
     solver = NearToeplitzSolver(N, nrhs, (1., 2., 1./4, 1., 1./4, 2., 1.))
@@ -26,8 +28,10 @@ for N, nrhs in zip(sizes, num_systems):
         solver.solve(d_d)
     end.record()
     end.synchronize()
-    table.add_row((N, nrhs, start.time_till(end)/(nsteps-1)))
-table.field_names = ('System size', 'Number of systems', 'Time to solve (ms)')
+    time_per_solve = (start.time_till(end)/(nsteps-1))*1e-3
+    throughput = (in_bytes+out_bytes)/(time_per_solve * 1e9)
+    table.add_row((N, nrhs, time_per_solve, throughput))
+table.field_names = ('System size', 'Number of systems', 'Time to solve (ms)', 'Bandwidth (GB/s)')
 table.align = 'l'
 
 print table
